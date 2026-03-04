632ef1a5a70d95ef9c803d62649b03f319daff9e
Neeraj <109914099+Neeraj10687@users.noreply.github.com>
2026-03-04 20:45:19 +0530

    max subarray

--- java/pp.java
+++ java/pp.java
@@ -1,24 +1,40 @@
 class pp{
 
   public static int rever(int num[]){
+    int sum=0;
+    int max=Integer.MIN_VALUE;
+    int[] pre = new int[num.length];
+
+    pre[0]=num[0];
+
+    for(int i=1;i<pre.length;i++){
+      pre[i]=pre[i-1]+num[i];
+    }
+
     for(int i=0;i<num.length;i++){
       for(int j=i;j<num.length;j++){
-        for(int k=i;k<=j;k++){
 
-          System.out.print("numbers "+ num[k]);
+         sum= i==0?pre[j]:pre[j]-pre[i-1];
+
+
 
         }
+        System.out.println("sum is "+sum);
+        if (max<sum) {
+          max=sum;
+          
+        }
         System.out.println();
+    System.out.println(max);
       }
-      System.out.println();
-    }
     return 0;
-  }
+    }
+  
 
 
   public static void main(String args[]) {
 
-    int num[]={1,2,3,4,6,7,9};
+    int num[]={2,4,6,8,10};
 
 
     int key=7;
