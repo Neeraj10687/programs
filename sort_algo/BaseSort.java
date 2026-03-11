efeed4fe8d43df718450cf80f4ce357c288f2e10
Neeraj <109914099+Neeraj10687@users.noreply.github.com>
2026-03-11 23:07:42 +0530

    ...

--- sort_algo/BaseSort.java
+++ sort_algo/BaseSort.java
@@ -37,6 +37,20 @@ class BaseSort{
       nums[i]=temp;
     }
   }
+  public static void I_sort(int nums[]){
+    for(int i=1;i<nums.length;i++){
+      int curr =nums[i];
+      int prev=i-1;
+
+      while(prev>=0&&nums[prev]>curr){
+        nums[prev+1]=nums[prev];
+        prev--;
+      }
+
+      //inseriton
+      nums[prev+1]=curr;
+    }
+  }
   public static void print(int nums[]) {
     for(int i=0;i<nums.length;i++){
       System.out.print(nums[i]+" ");
@@ -45,7 +59,7 @@ class BaseSort{
   }
   public static void main(String args[]){
     int[] nums={5,1,3,4,2};
-    S_sort(nums);
+    I_sort(nums);
     print(nums);
   }
 }
