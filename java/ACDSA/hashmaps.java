2f4dcc4e2f73287e30a3135df01cf3b574059e54
Neeraj <109914099+Neeraj10687@users.noreply.github.com>
2026-03-12 21:49:06 +0530

    hashmaps

---
+++ java/ACDSA/hashmaps.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class hashmaps{
+  public static void main(String args[]){
+    HashMap<String, Integer> hm=new HashMap<>();
+
+    //insert
+    hm.put("india",90);
+
+    System.out.println(hm);
+
+  }
+}
