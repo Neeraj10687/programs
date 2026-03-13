8933aa91d127b15fb065a7639552ae5729b8a1f3
Neeraj <109914099+Neeraj10687@users.noreply.github.com>
2026-03-13 22:29:23 +0530

    hashmaps

--- java/ACDSA/hashmaps.java
+++ java/ACDSA/hashmaps.java
@@ -4,9 +4,27 @@ class hashmaps{
     HashMap<String, Integer> hm=new HashMap<>();
 
     //insert
-    hm.put("india",90);
+    hm.put("india",80);
+    hm.put("pp",23);
+    hm.put("cambodia",50);
+    hm.put("canada",19);
+    hm.put("europe",30);
+    hm.put("US",97);
 
     System.out.println(hm);
+
+    Set<String>keys=hm.keySet();
+
+    System.out.println(keys);
+
+    for (String k : keys) {
+
+      System.out.println("k"+k+"value gotten"+hm.get(k));
+      
+    }
+
+
+
 
   }
 }
